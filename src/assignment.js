2424bb66ba08b2a4d1002a43b1bb0da4bbaedcee
Toheeb Tinuoye <104863148+tinuoye@users.noreply.github.com>
2022-07-05 08:02:41 +0000

    Challenge 2 and 3 edited to pass the test

--- src/assignment.js
+++ src/assignment.js
@@ -39,7 +39,7 @@ const assignment = {};
  * @returns number the count of even numbers
  */
 
-let arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20];
+let arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21];
 
 let arrayOfEvenNumbers = [];
 
